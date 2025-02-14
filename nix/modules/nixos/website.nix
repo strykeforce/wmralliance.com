74b7c6bdedb1ae4bb190c3e66a7415b145fc4c00
Jeff Hutchison <jeff@jeffhutchison.com>
2025-02-14 11:00:41 -0500

    Enable SSL

--- nix/modules/nixos/website.nix
+++ nix/modules/nixos/website.nix
@@ -12,12 +12,15 @@ in
   services.nginx = {
     enable = true;
     virtualHosts."www.wmralliance.com" = {
+      enableACME = true;
+      forceSSL = true;
       serverAliases = [
         "wmralliance.com"
         "wmralliance.strykeforce.org"
-
       ];
       locations."/".alias = "${flake.packages.${system}.wmralliance}/";
     };
   };
+
+  security.acme.certs."www.wmralliance.com".email = "jeff@j3ff.io";
 }
